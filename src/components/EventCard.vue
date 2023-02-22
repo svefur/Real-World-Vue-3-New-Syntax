e70daf67cb4e9f7c9f09e940f2ae26201fe0b46a
svefur <124247388+svefur@users.noreply.github.com>
2023-02-22 08:57:39 +0100

    testesting commit

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -15,6 +15,7 @@ defineProps({
     <div class="event-card">
       <h2>{{ event.title }}</h2>
       <span>@{{ event.time }} on {{ event.date }}</span>
+      test
     </div>
   </RouterLink>
 </template>
